445f44a295067e3d31ccae6697c11c80f6cd3a44
Ryan Gilfillan <regilfillan@gmail.com>
2019-05-21 17:35:44 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -8,18 +8,16 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    self.total += amount * quantity
+    self.total += price * quantity
+    quantity.times do 
+      items << title 
+    end
+    self.last_transaction= price * quantity
   end
-
+  
+  def
+  
 end
-
-#   def add_item(title, amount, quantity=1)
-#     self.total += amount * quantity
-#     quantity.times do
-#       items << title
-#     end
-#     self.last_transaction = amount * quantity
-#   end
 
 #   def apply_discount
 #     if discount != 0
